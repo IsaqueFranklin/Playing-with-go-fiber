e175695a1394617cb41392ce298763e980ecc6c4
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-08 19:11:29 -0300

    first play

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  //"fmt"
+  //"log" 
+  //"html/template"
+  "github.com/gofiber/template/html/v2"
+  "github.com/gofiber/fiber/v2"
+  //"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+    app := fiber.New(fiber.Config{
+      Views: html.New("./views", ".html"),
+    })
+
+    app.Static("/", "./public", fiber.Static{
+      Compress: true,
+    })
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.Render("index", fiber.Map{})
+    })
+
+    app.Get("/about", func(c *fiber.Ctx) error {
+      return c.Render("about", fiber.Map{})
+    })
+
+    app.Listen(":3000")
+}
